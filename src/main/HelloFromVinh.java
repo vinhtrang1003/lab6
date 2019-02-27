a9358ef5e18d6da548afc0df38180379eb3fee4e
Student <Student@A125363.ad.csueastbay.edu>
2019-02-27 12:26:44 -0800

    lab6 - tung

--- src/main/HelloFromVinh.java
+++ src/main/HelloFromVinh.java
@@ -15,6 +15,7 @@ public class HelloFromVinh {
        
         System.out.println("First Push from Vinh");
         System.out.println("Second Push from Surbir");
+        System.out.println("third Push from tung");
     
 }
 }
